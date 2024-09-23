59466d0faca866eabc12ec38ecee499dfc60cff8
dcosse <cosse@codelutin.com>
2024-09-23 23:27:41 +0200

    update CLICKABLE_FRAMEWORK

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CLICKABLE_FRAMEWORK=ubuntu-sdk-20.04 clickable build --arch arm64 
+CLICKABLE_FRAMEWORK=ubuntu-sdk-20.04.1 clickable build --arch arm64
 pkill -SIGHUP -f /Applications/Docker.app 'docker serve' 
  
