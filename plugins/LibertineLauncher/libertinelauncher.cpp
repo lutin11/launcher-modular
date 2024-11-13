f133abfb8de2ad266a16646a38910c0ced9e958b
dcosse <cosse@codelutin.com>
2024-11-13 23:42:11 +0100

    use lomiri-app-launch instead of libertine-launch

--- plugins/LibertineLauncher/libertinelauncher.cpp
+++ plugins/LibertineLauncher/libertinelauncher.cpp
@@ -10,15 +10,7 @@ LibertineLauncher::LibertineLauncher(QObject *parent) : QObject(parent) {}
 
 void LibertineLauncher::launchLibertineApp(const QString &containerName, const QString &appName)
 {
-    QThread *workerThread = new QThread;
     LibertineWorker *worker = new LibertineWorker(containerName, appName);
-
-    worker->moveToThread(workerThread);
-
-    connect(workerThread, &QThread::started, worker, &LibertineWorker::run);
-    connect(worker, &LibertineWorker::finished, workerThread, &QThread::quit);
-    connect(worker, &LibertineWorker::finished, worker, &LibertineWorker::deleteLater);
-    connect(workerThread, &QThread::finished, workerThread, &QThread::deleteLater);
-
-    workerThread->start();
+    
+    worker->run();
 }
--- plugins/LibertineLauncher/libertineworker.cpp
+++ plugins/LibertineLauncher/libertineworker.cpp
@@ -14,41 +14,19 @@ void LibertineWorker::run() {
         QString displayValue = env.value("DISPLAY", ":0");  // TODO set environement at launcher modular startup
         env.insert("DISPLAY", displayValue);
 
-        // Start XWayland libertineProcess synchronously
-        QProcess xWaylandProcess;
-        xWaylandProcess.setProcessEnvironment(env);
-        QStringList waylandParams;
-        waylandParams << "-once" << "-sigstop";
-        xWaylandProcess.start("Xwayland", waylandParams);
-
-        // Wait for XWayland to start
-        if (!xWaylandProcess.waitForStarted()) {
-            qDebug() << "Erreur : Impossible de démarrer XWayland";
-            emit finished();
-            return;
-        }
-        qDebug() << "XWayland démarré avec DISPLAY=" << displayValue;
-
-        // Launch libertine app after XWayland is ready
         QProcess libertineProcess;
         libertineProcess.setProcessEnvironment(env);
-        QStringList arguments;
-        arguments << "-i" << m_containerName << m_appName;
-        libertineProcess.start("libertine-launch", arguments);
+
+        libertineProcess.start("lomiri-app-launch", QStringList() << m_containerName + "_" + m_appName);
 
         if (!libertineProcess.waitForStarted()) {
-            qDebug() << "Erreur : Impossible de lancer l'application" << m_appName;
+            qDebug() << "Erreur : Failed to start application" << m_appName;
         } else {
-            qDebug() << "Lancement de l'application" << m_appName << "dans le conteneur" << m_containerName;
+            qDebug() << "Application " << m_appName << " starting into containers " << m_containerName;
         }
 
         // Wait for the app to finish, then stop XWayland
         libertineProcess.waitForFinished(-1);// avoid the 30-second timeout
-        if (xWaylandProcess.state() != QProcess::NotRunning) {
-            xWaylandProcess.terminate();
-            xWaylandProcess.waitForFinished();
-        }
-        qDebug() << "XWayland terminé";
 
         emit finished();
 }
