7d7fc3454868b9ee327c52c97533a735b03434e5
david <cosse@codelutin.com>
2025-09-18 09:46:15 +0200

    refs #9 refactor application starting

--- main.cpp
+++ main.cpp
@@ -8,20 +8,21 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication *app = new QGuiApplication(argc, (char**)argv);
-    app->setApplicationName("launchermodular.lut11");
-    QQmlEngine *engine = new QQmlEngine(app);
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+
+    QGuiApplication app(argc, argv);
+    app.setApplicationName("launchermodular.lut11");
+    QQmlEngine engine;
 
     // Set global debug property
     bool debug = false; // true to enable debug
-    engine->rootContext()->setContextProperty("DEBUG_MODE", debug);
-    engine->addImportPath(QStringLiteral("/usr/lib/aarch64-linux-gnu/lomiri/qml/"));
+    engine.rootContext()->setContextProperty("DEBUG_MODE", debug);
+    engine.addImportPath(QStringLiteral("/usr/lib/aarch64-linux-gnu/lomiri/qml/"));
 
-    QQuickView *view = new QQuickView(engine, nullptr);
+    QQuickView view(&engine, nullptr);
+    view.setSource(QStringLiteral("%1/qml/Main.qml").arg(app.applicationDirPath()));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.show();
 
-    view->setSource(QStringLiteral("%1/qml/Main.qml").arg(app->applicationDirPath()));
-    view->setResizeMode(QQuickView::SizeRootObjectToView);
-    view->show();
-
-    return app->exec();
+    return app.exec();
 }
